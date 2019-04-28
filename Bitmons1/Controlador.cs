9e98709ca3df701056bfe07d783300fc89757654
PedroNaretto <ppnaretto@miuandes.cl>
2019-04-28 19:45:07 -0300

    Metodo relacion entorno con bitmon

--- Bitmons1/Controlador.cs
+++ Bitmons1/Controlador.cs
@@ -8,26 +8,35 @@ namespace Bitmons1
 {
     public class Controlador
     {
-        Mapa cMapa = new Mapa();
-        Bitmons cBitmons = new Bitmons();
-
         public Controlador()
         {
         }
 
-        public void generarMapas(int filas, int columnas)
+        public void generarMapas(int filas, int columnas,Mapa cMapa, Bitmons cBitmons)
         {
             cMapa.GenerarMapa(filas, columnas);
             cBitmons.Spawn(filas, columnas);
         }
 
-        public void Entorno()
+        //Para cada bitmon, revisa si se encuentra en un terreno que le haga daño
+        public void Entorno(Mapa cMapa, Bitmons cBitmons)
         {
             for (int i = 0; i < cMapa.Mterrenos.GetUpperBound(0); i++)
             {
                 for (int j = 0; j < cMapa.Mterrenos.GetUpperBound(1); j++)
                 {
-
+                    foreach(Bitmon bitmon in cBitmons.bitmons_simulacion[i,j])
+                    {
+                        string t = cMapa.Mterrenos[i, j].getTerreno();
+                        if (bitmon.debilidad.Contains(t))
+                        {
+                            bitmon.TiempoVida -= 2;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
                 }
             }
         }
